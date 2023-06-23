08314ad4c7608fb5b15735e29c0ebe3e760faac7
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2023-06-23 23:25:08 +0200

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -7,19 +7,18 @@ from dotenv import load_dotenv
 load_dotenv()
 bot_token=os.getenv("BOT_TOKEN")
 
-
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send('Hello!')
+    await ctx.send('Hello, how can I help?')
 
 @bot.command()
 async def ask(ctx, *, question):
-    #url = 'http://127.0.0.1:8000/gpt'
-    url = ''
+    url = 'http://127.0.0.1:8000/gpt'
+    #url = ''
     data = {'user_input': question}
     headers = {'Content-Type': 'application/json'}
 
@@ -29,10 +28,10 @@ async def ask(ctx, *, question):
         response_json = response.json()
 
         if 'output' in response_json:
-            await ctx.send(f"{ctx.author.mention}, {response_json['output']}")
+            await ctx.reply(f"{response_json['output']}")
         else:
-            await ctx.send(f"{ctx.author.mention}, The 'output' key was not found in the API response.")
+            await ctx.reply(f"The 'output' key was not found in the API response.")
     else:
-        await ctx.send(f"{ctx.author.mention}, *Sad beep* - I'm sorry I couldn't understand your question, please ask me again.")
+        await ctx.reply(f"*Sad beep* - I'm sorry I couldn't understand your question, please ask me again.")
 
 bot.run(bot_token)
