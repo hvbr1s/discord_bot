fe7143d0f9d5c618ff0bc0f881326f754eeaf8a7
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2023-07-04 00:16:40 +0200

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -2,38 +2,46 @@ import discord
 import os
 import requests
 from discord.ext import commands
-from dotenv import load_dotenv
+from dotenv import main
 
-load_dotenv()
+main.load_dotenv()
 bot_token=os.getenv("BOT_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send('Hello, how can I help?')
-
-@bot.command()
-@commands.cooldown(1, 20, commands.BucketType.user)
-async def ask(ctx, *, question):
-    #url = 'http://127.0.0.1:8000/gpt'
-    url = ''
+async def answer_question(ctx, question):
+    #url = 'http://127.0.0.1:8008/gpt'
+    #url = 'https://34.163.86.35:80/gpt'
     data = {'user_input': question}
     headers = {'Content-Type': 'application/json'}
 
-    response = requests.post(url, json=data, headers=headers)
+    try:
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()  # raise an HTTPError if the response contains an HTTP error status code
 
-    if response.status_code == 200:
         response_json = response.json()
 
         if 'output' in response_json:
             await ctx.reply(f"{response_json['output']}")
         else:
             await ctx.reply(f"The 'output' key was not found in the API response.")
-    else:
-        await ctx.reply(f"*Sad beep* - I'm sorry I couldn't understand your question, please ask me again.")
+    except RequestException as e:
+        print(f"Error occurred while sending request: {e}")
+        await ctx.reply(f"*Sad beep* - I'm sorry I couldn't reach my knowledge base. Please try again later.")
+
+@bot.event
+async def on_message(message):
+    if bot.user.mentioned_in(message):
+        question = message.content.replace(f'<@!{bot.user.id}>', '').strip()  # extract question
+        await answer_question(message, question)
+    await bot.process_commands(message)
+
+@bot.command()
+@commands.cooldown(1, 20, commands.BucketType.user)
+async def ask(ctx, *, question):
+    await answer_question(ctx, question)
 
 @ask.error
 async def ask_error(ctx, error):
@@ -44,4 +52,3 @@ async def ask_error(ctx, error):
         raise error
 
 bot.run(bot_token)
-
