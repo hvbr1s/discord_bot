d11db622ada6e9e4814432abdf3a732a793d12e6
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2023-10-14 13:32:10 +0200

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -8,6 +8,7 @@ if not nltk.data.find('tokenizers/punkt'):
 from nltk.tokenize import word_tokenize
 from discord.ext import commands
 from dotenv import main
+import aiohttp
 
 main.load_dotenv()
 bot_token=os.getenv("BOT_TOKEN")
@@ -17,48 +18,46 @@ BITCOIN_ADDRESS_PATTERN = r'\b(1|3)[1-9A-HJ-NP-Za-km-z]{25,34}\b|bc1[a-zA-Z0-9]{
 LITECOIN_ADDRESS_PATTERN = r'\b(L|M)[a-km-zA-HJ-NP-Z1-9]{26,34}\b'
 DOGECOIN_ADDRESS_PATTERN = r'\bD{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}\b'
 XRP_ADDRESS_PATTERN = r'\br[a-zA-Z0-9]{24,34}\b'
-
-# BIP39 Filter
-
-def contains_bip39_phrase(message):
-    words = word_tokenize(message.lower())
-    bip39_words = [word for word in words if word in BIP39_WORDS]
-    return len(bip39_words) >= 23
-
-with open('bip39_words.txt', 'r') as file:
-    BIP39_WORDS = set(word.strip() for word in file)
+COSMOS_ADDRESS_PATTERN = r'\bcosmos[0-9a-z]{38,45}\b'
+SOLANA_ADDRESS_PATTERN= r'\b[1-9A-HJ-NP-Za-km-z]{32,44}\b'
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
 async def answer_question(ctx, question):
-    url = 'http://127.0.0.1:8008/gpt'
-    data = {'user_input': question, 'user_id': str(ctx.author.id)}
-    headers = {'Content-Type': 'application/json', "Authorization": f"Bearer {os.getenv('BACKEND_API_KEY')}"} #changed
+    url = 'http://127.0.0.1:8800/gpt'
+    data = {'user_input': question, 'user_id': str(ctx.author.id), 'user_locale':'eng'}
+    headers = {'Content-Type': 'application/json', "Authorization": f"Bearer {os.getenv('BACKEND_API_KEY')}"}
 
-    try:
-        response = requests.post(url, json=data, headers=headers) #changed
-        response.raise_for_status()  
-
-        response_json = response.json()
-
-        if 'output' in response_json:
-            await ctx.reply(f"{response_json['output']}")
-        else:
-            await ctx.reply(f"The 'output' key was not found in the API response.")
-    except requests.exceptions.RequestException as e:
-        print(f"Error occurred while sending request: {e}")
-        await ctx.reply(f"*Sad beep* - I'm sorry I couldn't reach my knowledge base. Please try again later.")
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(url, json=data, headers=headers) as response:
+                if response.status == 200:
+                    response_json = await response.json()
+                    if 'output' in response_json:
+                        await ctx.reply(f"{response_json['output']}")
+                    else:
+                        await ctx.reply(f"The 'output' key was not found in the API response.")
+                else:
+                    await ctx.reply(f"*Sad beep* - I'm sorry I couldn't reach my knowledge base. Please try again later.")
+        except Exception as e:
+            print(f"Error occurred while sending request: {e}")
+            await ctx.reply(f"*Sad beep* - I'm sorry I couldn't reach my knowledge base. Please try again later.")
 
 @bot.event
 async def on_message(message):
     if bot.user.mentioned_in(message):
         question = message.content.replace(f'<@!{bot.user.id}>', '').strip()  # extract question
-        if re.search(ETHEREUM_ADDRESS_PATTERN, question, re.IGNORECASE) or re.search(BITCOIN_ADDRESS_PATTERN, question, re.IGNORECASE) or re.search(LITECOIN_ADDRESS_PATTERN, question, re.IGNORECASE) or re.search(DOGECOIN_ADDRESS_PATTERN, question, re.IGNORECASE) or re.search(XRP_ADDRESS_PATTERN, question, re.IGNORECASE) :
+        if re.search(ETHEREUM_ADDRESS_PATTERN, question, re.IGNORECASE) or \
+           re.search(BITCOIN_ADDRESS_PATTERN, question, re.IGNORECASE) or \
+           re.search(LITECOIN_ADDRESS_PATTERN, question, re.IGNORECASE) or \
+           re.search(DOGECOIN_ADDRESS_PATTERN, question, re.IGNORECASE) or \
+           re.search(COSMOS_ADDRESS_PATTERN, question, re.IGNORECASE) or \
+           re.search(SOLANA_ADDRESS_PATTERN, question, re.IGNORECASE) or \
+           re.search(XRP_ADDRESS_PATTERN, question, re.IGNORECASE) :
             await message.reply("I'm sorry, but I can't assist with questions that crypto addresses. Please remove the address and ask again.")
-        elif contains_bip39_phrase(question):
-            await message.reply("It looks like you've included a recovery phrase in your message. Please never share your recovery phrase. It is the master key to your wallet and should be kept private.")
         else:
             await answer_question(message, question)
     await bot.process_commands(message)
